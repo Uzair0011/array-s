7a8737c8fc9a8942726626c59c1ed7f771bd2fec
Uzair Ahmed <159642641+Uzair0011@users.noreply.github.com>
2024-03-30 02:39:34 +0500

    upload

---
+++ main.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What is you favourite Hobbies",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        },
+    ]);
+    todos.push(addTask.todo);
+    //todos.pop();
+    condition = addTask.addMore;
+    console.log(todos);
+}
